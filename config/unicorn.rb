6eff6a90fd132cc79e0c556ab5716553282c0cbf
Devendra <devendra_rath@yahoo.com>
2015-02-25 22:12:42 +0530

    fix config issues

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,4 +1,4 @@
-root = "/home/deployer/apps/goodmorning/current"
+root = "/home/deployer/apps/GoodMorningWeb/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
