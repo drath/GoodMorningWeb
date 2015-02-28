9f11334e4f4602017ede36bb3a939378de291e9a
Devendra <devendra_rath@yahoo.com>
2015-02-28 11:49:27 +0530

    GCM testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 		logger.debug "Inside posts PostsController"
 		gcm = GCM.new("AIzaSyB8lrrC-q5ECkzPLmJ5Mi9fA03nZPzbii0")
 		registration_ids= ["APA91bHvVBpz8vmeKmoSutzpq-olxY9cAvfagqLu-l-H4ue2LQHjR76IgxhzxGzKglRNnPno6X5RB7fsXVXtpwt8PeuQoU9uG8GHGXIJU6jeBU8YPKT4oeMiNTeHf9rhFAFimgp3LY-XGBJKZx2DKDUpcX_AALAeXrGXWrD5PFNgGN9puwROHVE"]
-		options = {data: {message: "sdfsfdfsdfdf"}, collapse_key: "updated_score"}
+		options = {data: {message: "http://oddstuffmagazine.com/wp-content/uploads/2011/09/Small-Cat-580x574.png"}, collapse_key: "updated_score"}
 		response = gcm.send(registration_ids, options)
 		response.each do |key, value|
 		    logger.debug key 
