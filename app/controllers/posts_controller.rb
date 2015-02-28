a4fded8dc1965befbdff70a5c45785910f1a79e8
Devendra <devendra_rath@yahoo.com>
2015-02-28 10:54:10 +0530

    GCM testing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,14 +2,16 @@ require 'gcm'
 
 class PostsController < ApplicationController
 	def index
+		logger.debug "Inside posts PostsController"
 		gcm = GCM.new("AIzaSyB8lrrC-q5ECkzPLmJ5Mi9fA03nZPzbii0")
 		registration_ids= ["APA91bHvVBpz8vmeKmoSutzpq-olxY9cAvfagqLu-l-H4ue2LQHjR76IgxhzxGzKglRNnPno6X5RB7fsXVXtpwt8PeuQoU9uG8GHGXIJU6jeBU8YPKT4oeMiNTeHf9rhFAFimgp3LY-XGBJKZx2DKDUpcX_AALAeXrGXWrD5PFNgGN9puwROHVE"]
-		options = {data: {message: "123"}, collapse_key: "updated_score"}
+		options = {data: {message: "sdfsfdfsdfdf"}, collapse_key: "updated_score"}
 		response = gcm.send(registration_ids, options)
 		response.each do |key, value|
 		    logger.debug key 
 		    logger.debug value
 		end
-		render :text => "Response is..." + response[:response] 
+		logg
+		render :text => "WWWWWResponse is............" + response[:response] 
 	end
 end
