9cd2cc331909000ad8fbb3efe98db2f9011ef343
Devendra <devendra_rath@yahoo.com>
2015-02-28 16:37:09 +0530

    capistrano test

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 		logger.debug "Inside posts PostsController"
 		gcm = GCM.new("AIzaSyB8lrrC-q5ECkzPLmJ5Mi9fA03nZPzbii0")
 		registration_ids= ["APA91bHvVBpz8vmeKmoSutzpq-olxY9cAvfagqLu-l-H4ue2LQHjR76IgxhzxGzKglRNnPno6X5RB7fsXVXtpwt8PeuQoU9uG8GHGXIJU6jeBU8YPKT4oeMiNTeHf9rhFAFimgp3LY-XGBJKZx2DKDUpcX_AALAeXrGXWrD5PFNgGN9puwROHVE"]
-		options = {data: {message: "http://wallfinest.com/wp-content/uploads/2014/06/cute-cat-for-android-wallpaper.jpg"}, collapse_key: "updated_score"}
+		options = {data: {message: "http://upload.wikimedia.org/wikipedia/en/thumb/a/a6/Goofy.svg/690px-Goofy.svg.png"}, collapse_key: "updated_score"}
 		response = gcm.send(registration_ids, options)
 		response.each do |key, value|
 		    logger.debug key 
