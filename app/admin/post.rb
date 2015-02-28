1ed035a3637b44fd604647baf9ef05f56405f422
Devendra <devendra_rath@yahoo.com>
2015-02-28 23:37:28 +0530

    active

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -18,7 +18,7 @@ ActiveAdmin.register Post do
   end
 
   before_create do |post|
-    logger.debug "Creating a new post!"
+    logger.debug "Creating a new post...url is:" + post.picture.url(:medium)
     gcm = GCM.new("AIzaSyB8lrrC-q5ECkzPLmJ5Mi9fA03nZPzbii0")
     registration_ids= ["APA91bHvVBpz8vmeKmoSutzpq-olxY9cAvfagqLu-l-H4ue2LQHjR76IgxhzxGzKglRNnPno6X5RB7fsXVXtpwt8PeuQoU9uG8GHGXIJU6jeBU8YPKT4oeMiNTeHf9rhFAFimgp3LY-XGBJKZx2DKDUpcX_AALAeXrGXWrD5PFNgGN9puwROHVE"]
     options = {data: {message: "http://upload.wikimedia.org/wikipedia/commons/4/44/Jelly_cc11.jpg"}, collapse_key: "updated_score"}
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
 	has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
+	def picture_url
+		picture.url(:medium)
+	end
 end
