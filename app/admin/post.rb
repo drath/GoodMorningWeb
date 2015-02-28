d111aef9ada348343a610b6fba2821b7e379bef4
Devendra <devendra_rath@yahoo.com>
2015-03-01 00:21:55 +0530

    push image

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -17,7 +17,7 @@ ActiveAdmin.register Post do
 
   end
 
-  before_create do |post|
+  after_create do |post|
     logger.debug "Creating a new post...url is:" + post.picture.url(:medium)
     gcm = GCM.new("AIzaSyB8lrrC-q5ECkzPLmJ5Mi9fA03nZPzbii0")
     registration_ids= ["APA91bHvVBpz8vmeKmoSutzpq-olxY9cAvfagqLu-l-H4ue2LQHjR76IgxhzxGzKglRNnPno6X5RB7fsXVXtpwt8PeuQoU9uG8GHGXIJU6jeBU8YPKT4oeMiNTeHf9rhFAFimgp3LY-XGBJKZx2DKDUpcX_AALAeXrGXWrD5PFNgGN9puwROHVE"]
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
-	has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :picture,
+										:path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+										:url => "/system/:attachment/:id/:style/:filename",
+										:styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
+
 
 	def picture_url
 		picture.url(:medium)
